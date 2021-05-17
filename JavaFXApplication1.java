755ba6ad11a2f6783d1fae791db320da06d438e9
Pedro Neto <pedro@tuxcuiabano.com>
2021-05-17 15:58:27 -0400

    Alteração
    foi alterado a classe

--- JavaFXApplication1.java
+++ JavaFXApplication1.java
@@ -20,12 +20,6 @@ public class JavaFXApplication1 extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
 
     /**
      * @param args the command line arguments
